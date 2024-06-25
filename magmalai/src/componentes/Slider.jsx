b95ea2d410a3c39cc31cacef72afa351f331c083
mairamar <martinezmaira43c1@gmail.com>
2024-06-24 22:03:27 -0300

    actualizacion-registro

--- magmalai/src/componentes/Slider.jsx
+++ magmalai/src/componentes/Slider.jsx
@@ -1,29 +1,22 @@
-import mongo from "../imagenes/mongo.png";
-import express from "../imagenes/express.png";
-import react from "../imagenes/react.jpg";
-import node from "../imagenes/node.png";
-
+import './styles.css',
 export default [
-
-
     {
         title: "",
         description: "",
         urls: node,
     },
-    
     {
-        title: "",
+        title: "Somos una plataforma innovadora diseñada para revolucionar el estudio universitario.",
         description: "",
         urls: mongo,
     },
     {
-        title: "¿Cómo usar Malagmai?",
+        title: "Malagmai se centra en ofrecer una experiencia dinámica y efectiva para estudiantes mediante la autoevaluación de documentos PDF y Word.",
         description: "",
         urls: express,
     },
     {
-        title: "",
+        title: "Malagmai está dirigido a estudiantes universitarios que buscan mejorar su método de estudio y obtener mejores resultados académicos de manera más eficiente.",
         description: "",
         urls: react,
     },
